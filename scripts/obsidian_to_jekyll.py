45f30ac8208c948a46a79d7a24ff21f6fc56287f
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-08-22 16:00:19 +0200

    Add FrontMatter to site_src markdown files

--- scripts/obsidian_to_jekyll.py
+++ scripts/obsidian_to_jekyll.py
@@ -3,6 +3,7 @@
 import argparse
 from pathlib import Path
 import shutil
+import re
 
 
 def main():
@@ -11,7 +12,7 @@ def main():
     argument_parser.add_argument('--vault', default='absurdly-goud-obsidian')
     argument_parser.add_argument('--src-root', default='.')
     argument_parser.add_argument('--out-dir', default='site_src',
-                   help='If provided, generate the site source into this directory and do NOT sync into the repo')
+                                 help='If provided, generate the site source into this directory and do NOT sync into the repo')
     arguments = argument_parser.parse_args()
 
     obsidian_vault_location, output_location, source_location = extract_command_line_arguments(arguments)
@@ -23,12 +24,40 @@ def main():
     if output_location.exists():
         shutil.rmtree(output_location)
 
-    copy_obsidian_vault(obsidian_vault_location, output_location)
+    copy_vault_resources(obsidian_vault_location, output_location)
+    copy_jekyll_resources(source_location, output_location)
 
-    include_items = ['assets', 'CNAME', 'posts', '_includes', 'layouts', '_config.yaml' ,'index.md']
+    # recursive glob (rglob) will find all markdown files for us
+    for markdown_file in output_location.rglob('*.md'):
+        add_frontmatter_to_file(markdown_file)
+
+
+def add_frontmatter_to_file(markdown_file, file_layout='default'):
+    file_title = extract_title_from_file_name(markdown_file.name)
+    file_path = Path(markdown_file)
+    file_content = file_path.read_text(encoding="utf-8")
+
+    frontmatter = f"""---
+layout: {file_layout}
+title: {file_title}
+---
+
+"""
+    file_path.write_text(frontmatter + file_content, encoding="utf-8")
+
+def extract_title_from_file_name(file_name: str) -> str:
+    filename_with_leading_timestamp = re.compile(r'^\d{4}-\d{2}-\d{2}-')
+    filename_ending_in_md = re.compile(r'.md')
+    file_title = filename_with_leading_timestamp.sub('', file_name)
+    file_title = filename_ending_in_md.sub('', file_title)
+    return file_title
+
+
+def copy_jekyll_resources(source_location: Path, output_location: Path):
+    included_items = ['assets', 'CNAME', 'posts', '_includes', 'layouts', '_config.yaml', 'index.md']
     for source_item in source_location.iterdir():
         source_item_name = source_item.name
-        if source_item_name not in include_items:
+        if source_item_name not in included_items:
             continue
         print(f"Copying '{source_item_name}' to '{output_location}/{source_item_name}'")
         if source_item.is_dir():
@@ -37,8 +66,7 @@ def main():
             shutil.copy2(source_item, output_location / source_item_name)
 
 
-
-def copy_obsidian_vault(obsidian_vault_location, output_location):
+def copy_vault_resources(obsidian_vault_location: Path, output_location: Path):
     ignored_items = ['.obsidian']
     print(f"ignored_items: '{ignored_items}'")
     for vault_item in obsidian_vault_location.iterdir():
